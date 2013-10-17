8c82be288979143bf8c6d0908f3bc8965c8529dd
Tales <tales@ubuntu.ubuntu-domain>
2013-10-17 15:20:23 -0300

    correcao na msg de add usuário

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -31,7 +31,7 @@
         if ($this->request->is('post')) {
             $this->User->create();
             if ($this->User->save($this->request->data)) {
-                $this->Session->setFlash(__('<script> alert("Usuario salvo com sucesso!"); </script>', true));
+                $this->Session->setFlash(__('<script> alert("Usuário salvo com sucesso!"); </script>', true));
                 $this->redirect(array('action' => 'add'));
             } else {
                $this->Session->setFlash(__('<script> alert("O usuário não pode ser salvo."); </script>', true));
